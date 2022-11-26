139a01d1c044a864a078e74b4b3e3edbabaa7571
Lee Harin <harinboy@snu.ac.kr>
2022-11-26 09:40:24 +0000

    Tests done on INT8/INT4 quantized model

--- fquant8/fquant.py
+++ fquant8/fquant.py
@@ -1,13 +1,19 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
+import copy
 
 class QuantConv1d(nn.Module):
 
-    def __init__(self, conv1d, k = 8):
-        super().__init__()
-        self.nf = conv1d.nf
+    def __init__(self, conv1d, k = 8, linear = False):
+        super(QuantConv1d, self).__init__()
         twok = (1<<(k-1))
-        w0 = conv1d.weight.data
+        if linear:
+            w0 = torch.transpose(conv1d.weight.data, 0, 1)
+            self.nf = conv1d.out_features
+        else:
+            w0 = conv1d.weight.data
+            self.nf = conv1d.nf
         m, _ = torch.min(w0, dim = 0)
         M, _ = torch.max(w0, dim = 0)
         s = torch.where(M < -m, m, M)/twok
@@ -15,27 +21,66 @@ class QuantConv1d(nn.Module):
         w = torch.round((torch.clamp(w0/s.expand(w0.size(0), -1), -twok, twok-1))).type(torch.int8)
         self.scale = nn.Parameter(s)
         self.weight = nn.Parameter(w, requires_grad = False)
-        self.bias = nn.Parameter(conv1d.bias.clone().detach())
+        if conv1d.bias != None:
+            self.bias = nn.Parameter(conv1d.bias.clone().detach())
+        else:
+            self.register_parameter('bias', None)
     
     def forward(self, x):
         size_out = x.size()[:-1] + (self.nf,)
         x = torch.mm(x.view(-1, x.size(-1)), self.weight.type(x.dtype))
         x = x * self.scale.expand(x.size(0), -1)
-        x = torch.add(self.bias, x)
+        if self.bias != None:
+            x = torch.add(self.bias, x)
         x = x.view(size_out)
         return x
 
-def FakeQuantize(model):
-    for block in model.transformer.h:
+class QuantLinear(nn.Module):
+    def __init__(self, linear, k = 8):
+        super(QuantLinear, self).__init__()
+        self.in_features = linear.in_features
+        self.out_features = linear.out_features
+        twok = (1<<(k-1))
+        w0 = torch.transpose(linear.weight.data, -2, -1)
+        m, _ = torch.min(w0, dim = 0)
+        M, _ = torch.max(w0, dim = 0)
+        s = torch.where(M<-m, m, M)/twok
+        s = s.reshape(1, -1)
+        w = torch.round((torch.clamp(w0/s.expand(w0.size(0), -1), -twok, twok-1))).type(torch.int8)
+        self.scale = nn.Parameter(s)
+        self.weight = nn.Parameter(w, requires_grad = False)
+        if linear.bias:
+            self.bias = nn.Parameter(linear.bias.clone().detach())
+        else:
+            self.register_parameter('bias', None)
+
+    def forward(self, x):
+        size_out = x.size()[:-1] + (self.out_features,)
+        x = torch.mm(x.view(-1, x.size(-1)), self.weight.type(x.dtype))
+        x = x * self.scale.expand(x.size(0), -1)
+        if self.bias:
+            x = torch.add(self.bias, x)
+        x = x.view(size_out)
+        return x
+
+
+
+def FakeQuantize(model, kbits = None, LastLinear = False):
+    qmodel = copy.deepcopy(model)
+    if kbits == None:
+        kbits = [[8] * 4 for _ in range(len(qmodel.transformer.h))]
+    for i, block in enumerate(qmodel.transformer.h):
         attn = block.attn
         if attn.is_cross_attention:
-            attn.q_attn = QuantConv1d(attn.q_attn)
-        attn.c_attn = QuantConv1d(attn.c_attn)
-        attn.c_proj = QuantConv1d(attn.c_proj)
+            attn.q_attn = QuantConv1d(attn.q_attn, kbits[i][5])
+        attn.c_attn = QuantConv1d(attn.c_attn, kbits[i][0])
+        attn.c_proj = QuantConv1d(attn.c_proj, kbits[i][1])
         mlp = block.mlp
-        mlp.c_fc = QuantConv1d(mlp.c_fc)
-        mlp.f_proj = QuantConv1d(mlp.c_proj)
-    return model
+        mlp.c_fc = QuantConv1d(mlp.c_fc, kbits[i][2])
+        mlp.c_proj = QuantConv1d(mlp.c_proj, kbits[i][3])
+    if LastLinear:
+        qmodel.lm_head = QuantConv1d(qmodel.lm_head, linear = True)
+    return qmodel
 
 
 '''
--- fquant8/fquant_test.py
+++ fquant8/fquant_test.py
@@ -1,20 +1,43 @@
 import sys
 from os import path
 sys.path.insert(0, path.abspath('..'))
-from test.testftns import TestLasttoken, ReadJson
+from test.testftns import TestLasttoken, Lambada, ReadJson
 from fquant import FakeQuantize
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from datasets import load_dataset
 
 import torch
 import torch.nn as nn
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-
+from torch.utils.data import DataLoader
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
-model = GPT2LMHeadModel.from_pretrained('gpt2').to(device).half()
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-FakeQuantize(model)
-openai_lambada = ReadJson("/home/cid2/dataset/lambada_test.jsonl", batch_size = 64)
-TestLasttoken(model, tokenizer, openai_lambada, verbose = True)
+path = "results_fquant_int8.txt"
+#dataset_lambada = Lambada(batch_size=32)
+openai_lambada = ReadJson("/home/cid2/dataset/lambada_test.jsonl", batch_size=32)
 
+tokenizer_small = GPT2Tokenizer.from_pretrained('gpt2')
+gpt2_small_model = GPT2LMHeadModel.from_pretrained('gpt2').to(device)
+gpt2_small_model.half()
 
+gpt2_fake_quant_model = FakeQuantize(gpt2_small_model)
+TestLasttoken(gpt2_fake_quant_model, tokenizer_small, openai_lambada, verbose = False, comment = "INT8 Fake Quantized GPT-2 small openai lambada", path = path)
+
+gpt2_fake_quant_model = FakeQuantize(gpt2_small_model, LastLinear = True)
+TestLasttoken(gpt2_fake_quant_model, tokenizer_small, openai_lambada, verbose = False, comment = "INT8 Fake Quantized GPT-2 small + output linear layer openai lambada", path = path)
+
+gpt2_fake_quant_model = FakeQuantize(gpt2_small_model, kbits = [[4]*4 for _ in range(12)])
+TestLasttoken(gpt2_fake_quant_model, tokenizer_small, openai_lambada, verbose = False, comment = "INT4 Fake Quantized GPT-2 small openai lambada", path = path)
+
+tokenizer_xl = GPT2Tokenizer.from_pretrained('gpt2-xl')
+gpt2_xl_model = GPT2LMHeadModel.from_pretrained('gpt2-xl').to(device)
+gpt2_xl_model.half()
+
+gpt2_fake_quant_model = FakeQuantize(gpt2_xl_model)
+TestLasttoken(gpt2_fake_quant_model, tokenizer_xl, openai_lambada, verbose = False, comment = "INT8 Fake Quantized GPT-2 xl openai lambada", path = path)
+
+gpt2_fake_quant_model = FakeQuantize(gpt2_xl_model, LastLinear = True)
+TestLasttoken(gpt2_fake_quant_model, tokenizer_xl, openai_lambada, verbose = False, comment = "INT8 Fake Quantized GPT-2 xl + output linear layer openai lambada", path = path)
+
+gpt2_fake_quant_model = FakeQuantize(gpt2_xl_model, kbits = [[4]*4 for _ in range(48)])
+TestLasttoken(gpt2_fake_quant_model, tokenizer_xl, openai_lambada, verbose = False, comment = "INT4 Fake Quantized GPT-2 xl openai lambada", path = path)
